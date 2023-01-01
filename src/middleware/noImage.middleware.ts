4c6b374ad4a425bd55ba86870d547ed1ecd38c22
hlngoben021 <hlngoben021@student.wethinkcode.co.za>
2023-01-01 15:50:42 +0200

    newC

--- src/middleware/noImage.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-// this will show if the page is not loaded
-export const norPageFound404 = (req: Request, res: Response) => {
-  res.status(404);
-  res.render('pageNotFound');
-};
